6737c9325f09b80abbf1bfa9dc4c3669fc051269
Ferdelicious <ashinofre@gmail.com>
2026-01-17 04:43:03 +0800

    Finished about.html
    Created a modal (main menu)

--- src/script.js
+++ src/script.js
@@ -17,3 +17,26 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+
+
+const openBtn = document.getElementById("openmodal");
+const closeBtn = document.getElementById("closemodal");
+const modal = document.getElementById("modal");
+
+openBtn.addEventListener("click", () => {
+  modal.classList.add("open");
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("open");
+});
+
+function closeModal() {
+  modal.classList.remove("open");
+}
+
+document.querySelectorAll("#modal a").forEach(link => {
+  link.addEventListener("click", () => {
+    closeModal();
+  });
+});
