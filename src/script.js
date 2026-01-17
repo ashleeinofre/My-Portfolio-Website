da72ba6316862a92bacaec6c7f004ca066f50f97
Ferdelicious <ashinofre@gmail.com>
2026-01-18 02:47:38 +0800

    Created skills.html
    Created projects.html
    added swiper js for the skills section

--- src/script.js
+++ src/script.js
@@ -40,3 +40,33 @@ document.querySelectorAll("#modal a").forEach(link => {
     closeModal();
   });
 });
+
+
+
+
+
+new Swiper(".swiper", {
+  loop: true,
+  spaceBetween: 30,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    },
+  }
+});
